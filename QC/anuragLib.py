209041ab10d8781357717f9c9b84875b14915404
Anurag Chaudhary <anuragchaudharyM87@gmail.com>
2025-01-21 17:15:36 +0530

    final

--- QC/anuragLib.py
+++ QC/anuragLib.py
@@ -1,10 +1,17 @@
+import numpy as np
+
 class SingleQubitTomography:
     def __init__(self, Nh=0, Nv=0, Nl=0, Nd=0):
         self.Nh = Nh
         self.Nv = Nv
         self.Nl = Nl
         self.Nd = Nd
+        self.Ph = 0
+        self.Pl = 0
+        self.Pd = 0
+        self.Pv = 0
 
+    # Getter and Setter for Nh, Nv, Nl, Nd
     def get_Nh(self):
         return self.Nh
 
@@ -29,19 +36,53 @@ class SingleQubitTomography:
     def set_Nd(self, Nd):
         self.Nd = Nd
 
-    def inputData(self):
-        print(f"input Data: \nNh={self.get_Nh()}\nNv={self.get_Nv()}\nNl={self.get_Nl()}\nNd={self.get_Nd()}")
+    # Getter and Setter for Probabilities (Ph, Pl, Pd, Pv)
+    def set_Ph(self):
+        self.Ph = self.Nh / self.Ntotal() if self.Ntotal() > 0 else 0
 
+    def get_Ph(self):
+        return self.Ph
+
+    def set_Pl(self):
+        self.Pl = self.Nl / self.Ntotal() if self.Ntotal() > 0 else 0
+
+    def get_Pl(self):
+        return self.Pl
+
+    def set_Pd(self):
+        self.Pd = self.Nd / self.Ntotal() if self.Ntotal() > 0 else 0
+
+    def get_Pd(self):
+        return self.Pd
+
+    def set_Pv(self):
+        self.Pv = self.Nv / self.Ntotal() if self.Ntotal() > 0 else 0
+
+    def get_Pv(self):
+        return self.Pv
+
+    # Display input data
+    def inputData(self):
+        print(f"Input Data: \nNh={self.get_Nh()}\nNv={self.get_Nv()}\nNl={self.get_Nl()}\nNd={self.get_Nd()}")
+
+    # Calculate total measurements
     def Ntotal(self):
         return self.Nh + self.Nv
 
+    # Calculate probabilities
     def probOfVectors(self):
-        Ph = self.Nh / self.Ntotal()
-        Pl = self.Nl / self.Ntotal()
-        Pd = self.Nd / self.Ntotal()
-        print(f"Ph={Ph:.3f}\nPl={Pl:.3f}\nPd={Pd:.3f}")
-        return round(Ph, 3), round(Pl, 3), round(Pd, 3)
-    
+        self.set_Ph()
+        self.set_Pl()
+        self.set_Pd()
+        self.set_Pv()
+        Ph = self.get_Ph()
+        Pl = self.get_Pl()
+        Pd = self.get_Pd()
+        Pv = self.get_Pv()
+        print(f"Probabilities:\nPh={Ph:.3f}\nPl={Pl:.3f}\nPd={Pd:.3f}\nPv={Pv:.3f}")
+        return round(Ph, 3), round(Pl, 3), round(Pd, 3), round(Pv, 3)
+
+    # Set all vector values
     def setAllVectors(self):
         print("Enter the values for Nh, Nv, Nl, Nd")
         inputNh = int(input("Nh: "))
@@ -54,5 +95,11 @@ class SingleQubitTomography:
         self.set_Nd(inputNd)
         print("Values set successfully")
 
-
-
+    # Generate a column matrix
+    def generateMatrix(self):
+        self.probOfVectors()
+        values = [1, 2 * self.Nh - 1, 2 * self.Nl - 1, 2 * self.Nd - 1]
+        matrix = np.array(values).reshape(-1, 1)
+        print("Generated Matrix:")
+        print(matrix)
+        return matrix
